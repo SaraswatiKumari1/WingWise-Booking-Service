29762a1e73e4480a29240686056c401e316d6b51
SaraswatiKumari1 <kri.saras123@gmail.com>
2025-11-08 18:58:19 +0530

    Added basic sequelize structure

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,4 @@
-const dotenv = require('dotenv'); //The require() function is a built-in Node.js method used to import external modules (packages or files).Here, it loads the npm package named dotenv
-
-
-
-dotenv.config(); //config will read your .env file, parse the contents, assign it to process.env, and return an     Object with a parsed key containing the loaded content or an error key if it failed.
-
-//dotenv->Object, and dotenv.config() returns an object with parsed variables.It also adds those variables to process.env
-
 module.exports = {
-    PORT: process.env.PORT
+    ServerConfig: require('./server-config'),
+    LoggerConfig: require('./logger-config')
 }
