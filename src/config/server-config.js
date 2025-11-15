b32680fdf365cb463c27ae20238ed185d8f39b67
SaraswatiKumari1 <kri.saras123@gmail.com>
2025-11-15 18:41:33 +0530

    Basic set up of folder structure is done

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv'); //The require() function is a built-in Node.js
 dotenv.config(); //config will read your .env file, parse the contents, assign it to process.env, and return an     Object with a parsed key containing the loaded content or an error key if it failed.
 
 //dotenv->Object, and dotenv.config() returns an object with parsed variables.It also adds those variables to process.env
-
+console.log("Port",process.env.PORT); //process is a global object in Node.js that provides information about, and control over, the current Node.js process. process.env is an object that contains the user environment.
 module.exports = {
     PORT: process.env.PORT
 }
