ae2787d153dc2e18dbcfc100c152e69c8de4784a
SaraswatiKumari1 <kri.saras123@gmail.com>
2025-11-15 20:38:15 +0530

    Added booking model with utils from WingWise

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+  ECONOMY: 'economy',
+  BUSINESS: 'business',
+  FIRST_CLASS: 'first_class',
+  PREMIUM_ECONOMY: 'premium_economy'
+};
+
+const BOOKING_STATUS = {
+  PENDING: 'pending',
+  BOOKED: 'booked',
+  CANCELLED: 'cancelled',
+  INITIATED: 'initiated'
+};
+
+module.exports = {
+  SEAT_TYPE,
+  BOOKING_STATUS
+};
